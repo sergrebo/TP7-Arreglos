650f9a56c29e0ecb47f1d31b9a170cda77cec1a9
sergrebo <sergio.rebolledo@est.fi.uncoma.edu.ar>
2022-10-26 09:02:04 -0300

    Create leerTemperaturas.php
    ejercicio 1a

---
+++ leerTemperaturas.php
@@ -0,0 +1,30 @@
+<?php
+/**Solicita al usuario las n temperaturas y las almacena en un arreglo. Retorna el arreglo formados
+ * @param FLOAT $temperatura
+ * @return ARRAY
+ */
+function leerTemperaturas($temperatura) {
+    //ARRAY $misTemperaturas STRING $otraTemp INT $i
+    $i=0;
+    $misTemperaturas[0]=$temperatura;
+    echo "¿Desea ingresar otra temperatura? s/n: ";
+    $otraTemp=trim(fgets(STDIN));
+    do {
+        $i=$i+1;
+        echo "Ingrese la temperatura: ";
+        $temperatura=trim(fgets(STDIN));
+        $misTemperaturas[$i]=$temperatura;
+        echo "¿Desea ingresar otra temperatura? s/n: ";
+        $otraTemp=trim(fgets(STDIN));
+    }
+    while ($otraTemp=="s");
+    return $misTemperaturas;
+}
+
+/*PROGRAMA PRINCIPAL*/
+/*FLOAR $temp ARRAY $arreglo*/
+echo "Ingrese la temperatura: ";
+$temp=trim(fgets(STDIN));
+$arreglo=leerTemperaturas($temp);
+print_r($arreglo);
+echo "\n FIN";
